a455ed3a079af63b8e35613c7a23a641375b0727
jbroadway <johnny@johnnybroadway.com>
2012-03-31 00:36:54 -0500

    Removed Symfony 2 and added symfony-standard from https://github.com/symfony/symfony-hello-world

--- symfony-2.0.12/app/cache/dev/assetic/config/1/199f390016a4d4852ad844d4844b5b80.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// TestHelloBundle:Default:index.html.twig
-return array (
-);
--- symfony-2.0.12/src/Test/HelloBundle/TestHelloBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Test\HelloBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TestHelloBundle extends Bundle
-{
-}
