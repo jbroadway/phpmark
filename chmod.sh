4fd954ad123db1df47e7278995045d9a8e42200c
Johnny Broadway <johnny@johnnybroadway.com>
2012-04-17 12:41:20 -0500

    Fixed chmod path for CI cache folder

--- chmod.sh
+++ chmod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod -R 0777 codeigniter-*/system/cache
+chmod -R 0777 codeigniter-*/application/cache
 chmod -R 0777 symfony-*/cache
 chmod -R 0777 symfony-*/app/cache
 chmod -R 0777 symfony-*/app/logs
